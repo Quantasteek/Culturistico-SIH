2a2bb453e8a04cd4a1fc1565a5d839621c9d2a05
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2024-10-10 17:11:48 +0530

    Add more responsiveness to nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,35 +10,32 @@ const Navbar = () => {
     setMobileDrawerOpen(!mobileDrawerOpen);
   };
   return (
-    <nav className="bg- sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700 ">
+    <nav className="flex space-between py-5 px-7 h-20 m-6 bg-gray-400 items-center rounded-lg z-50 backdrop-filter backdrop-blur-lg border border-neutral-700 ">
       <div className="container px-4 mx-auto relative text-sm">
         <div className="flex justify-between sp items-center">
           <div className="flex items-center space-around flex-shrink-0">
-            <img className="h-10 w-10 mr-2" src={logo} alt=" " />
+            <img className="h-10 w-10 mr-2" src={logo} alt="logo" />
             <span className="text-xl tracking-tight font-extrabold ">Culturistico</span>
-            <ul className="hidden space-between lg:flex lg:items-center lg:ml-28 lg:w-full space-x-12">
+            <ul className="hidden space-between lg:flex lg:items-center lg:ml-28 w-full space-x-12">
               {navItems.map((item, index) => (
                 <li className="font-bold text-base items-center hover:bg-black hover:text-white
                 transition-all h-fit hover:rounded-md hover:p-2" key={index}>
                   <a className="items-center" href={item.href}>{item.label}</a>
                 </li>
               ))}
-              <div className="hidden lg:flex justify-between space-x-12 mx-60 items-center">
-                <a href="#" className="py-2 px-3 border rounded-md ">
+              <div className=" hidden lg:flex justify-between space-x-12 mx-60 items-center">
+                <a href="#" className="py-2 px-3 border border-black rounded-md bg-gradient-to-r from-orange-500 to bg-orange-800 text-white font-semibold ">
                   Sign In
                 </a>
                 <a
                   href="#"
-                  className="text-white bg-gradient-to-r from-orange-500 to bg-orange-800 py-2 px-3 rounded-md "
+                  className="text-white bg-gradient-to-r from-orange-500 to bg-orange-800 py-2 px-3 rounded-md border border-black font-semibold"
                 >
                   Create an account
                 </a>
-              </div>
+              </div> 
             </ul>
-            {/* <div className='hidden lg:flex justify-between space-x-12 mx-60 items-center'>
-                        <a href="#" className='py-2 px-3 border rounded-md '>Sign In</a>
-                        <a href="#" className='bg-gradient-to-r from-orange-500 to bg-orange-800 py-2 px-3 rounded-md'>Create an account</a>
-                    </div> */}
+            
           </div>
           <div className="lg:higgen md:flex flex-col justify-end lg:relative lg:top-0">
             <button onClick={toggleNavbar} className="lg:hidden">
